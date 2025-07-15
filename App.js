3427a31698007c9f8b8233e92b22857e0c0c252c
AJINKYA <ajinkyacodes@gmail.com>
2025-07-15 11:39:30 +0530

    Added status bar

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar, useColorScheme } from 'react-native';
 import DrawerNavigator from './navigation/DrawerNavigator';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 export default function App() {
+
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <ThemeProvider>
       <NavigationContainer>
+        <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
         <DrawerNavigator />
       </NavigationContainer>
     </ThemeProvider>
